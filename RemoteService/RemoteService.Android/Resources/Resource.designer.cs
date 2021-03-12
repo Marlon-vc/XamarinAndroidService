a00a8d8cc5cda64f9daaaba857b78c2b71a406a2
Marlon Vega C <mvegach2013@gmail.com>
2021-03-12 12:02:06 -0600

    Agregados íconos custom

--- RemoteService/RemoteService.Android/Resources/Resource.designer.cs
+++ RemoteService/RemoteService.Android/Resources/Resource.designer.cs
@@ -14478,13 +14478,19 @@ namespace RemoteService.Droid
 			public const int roundedbgdark = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int test_custom_background = 2131165329;
+			public const int shield = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int tooltip_frame_dark = 2131165330;
+			public const int shield1 = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int tooltip_frame_light = 2131165331;
+			public const int test_custom_background = 2131165331;
+			
+			// aapt resource value: 0x7F070094
+			public const int tooltip_frame_dark = 2131165332;
+			
+			// aapt resource value: 0x7F070095
+			public const int tooltip_frame_light = 2131165333;
 			
 			static Drawable()
 			{
