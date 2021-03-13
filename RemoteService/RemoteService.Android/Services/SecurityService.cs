60c0fff6fda7d81c268df7edbaa4d29ac7a22eb9
Juan Chinchilla <juan.chinchilla@xerasistemas.net>
2021-03-12 21:26:05 -0600

    Método para mostrar notificación de alerta, y poder cancelarla

--- RemoteService/RemoteService.Android/Services/SecurityService.cs
+++ RemoteService/RemoteService.Android/Services/SecurityService.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 
@@ -25,6 +26,8 @@ namespace RemoteService.Droid.Services
         static readonly string TAG = Constants.TAG;
         private Messenger messenger;
         private bool isStarted;
+
+        private CancellationTokenSource alertCancellation;
 
         public override void OnCreate()
         {
@@ -63,7 +66,15 @@ namespace RemoteService.Droid.Services
 
             if (isStarted)
             {
+                var action = intent.Action;
+
+                if (action == "CANCEL_ALERT")
+                {
+                    alertCancellation?.Cancel();
+                }
+
                 Log.Info(Constants.TAG, "OnStartCommand() - Service already started");
+
             } 
             else
             {
@@ -105,6 +116,7 @@ namespace RemoteService.Droid.Services
                 
                 .SetContentTitle("Security Service")
                 .SetContentText("Rest assured, service is running")
+                //.SetProgress(100, 50, true)
                 .SetOngoing(true)
                 .Build();
         }
@@ -117,6 +129,55 @@ namespace RemoteService.Droid.Services
             var location = await Geolocation.GetLocationAsync(locationRequest);
 
             return $"Lat: {location.Latitude} Lng: {location.Longitude}";
+        }
+
+        public void SendAlert()
+        {
+            StartProgress().ConfigureAwait(false);
+        }
+
+        private async Task StartProgress(int secs = 10)
+        {
+            var notificationId = 65324;
+            var manager = (NotificationManager)GetSystemService(NotificationService);
+
+            alertCancellation = new CancellationTokenSource();
+
+            int i = 0;
+
+            while (i <= secs && !alertCancellation.Token.IsCancellationRequested)
+            {
+                var intent = new Intent(this, typeof(SecurityService));
+                intent.SetAction("CANCEL_ALERT");
+                var pendingIntent = PendingIntent.GetService(this, 0, intent, PendingIntentFlags.OneShot);
+                var cancelAction = new NotificationCompat.Action(0, "Cancelar", pendingIntent);
+
+                var notification = new NotificationCompat.Builder(this, ChannelId)
+                    .SetSmallIcon(Resource.Drawable.shield1)
+                    .AddAction(cancelAction)
+                    .SetContentTitle("Enviando alerta")
+                    .SetContentText("Te encuentras bien? Vamos a enviar una alerta.")
+                    .SetProgress(secs, i, false)
+                    .SetOngoing(true)
+                    .Build();
+
+                manager.Notify(notificationId, notification);
+
+                i++;
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+
+            if (!alertCancellation.Token.IsCancellationRequested)
+            {
+                // Enviar alerta
+                Toast.MakeText(this, "Alerta enviada!", ToastLength.Long).Show();
+            }
+            else
+            {
+                Toast.MakeText(this, "Alerta CANCELADA!", ToastLength.Long).Show();
+            }
+
+            manager.Cancel(notificationId);
         }
     }
 }
